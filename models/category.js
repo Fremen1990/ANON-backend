dacf107fef5cf2990a69ad8565408e1ec8777b1d
Fremen1990 <fremen1990@gmail.com>
2021-10-20 00:44:39 +0200

    Add category feature

---
+++ models/category.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+
+// Category Schema
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        maxLength: 32
+    },
+
+
+}, {timestamps: true});
+
+
+module.exports = mongoose.model("Category", categorySchema);
